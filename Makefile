a883d21c5c17043fe71745375280fd0de760aebf
Siddhant Kameswar <115331356+grimsteel@users.noreply.github.com>
2025-01-13 13:14:30 -0600

    stuff

--- Makefile
+++ Makefile
@@ -1,8 +1,13 @@
 solitaire: solitaire.o
 	ld -o $@ $^ -s -z noseparate-code  && strip --strip-section-headers $@
+solitaire-debug: solitaire-debug.o
+	ld -o $@ $^
 
 solitaire.o: solitaire.asm
 	nasm -f elf64 $^ -o $@
+
+solitaire-debug.o: solitaire.asm
+	nasm -f elf64 $^ -o $@ -g
 
 clean:
 	rm -f solitaire solitaire.o
--- solitaire.asm
+++ solitaire.asm
@@ -7,55 +7,83 @@ struc termios
 .c_lflag: resb 12
   resb 44
 endstruc
+
+%define TCGETS 21505
+%define TCSETS 21506
   
 global _start
 section .text
 _start:
   ; disable terminal line buffering
-  xor esi, esi
+  mov esi, TCGETS
   call tcgetorset
   and dword [termios_buf + termios.c_lflag], ~(0b1010) ; CANON + ECHO
-  xor esi, esi
   inc esi
   call tcgetorset
+
+main_loop:
+  mov esi, welcome
+  mov edx, welcome_len
+  call print
+
+  call getc
+  sub eax, 'q'
+  jz exit
+
+game_loop:
   
-  xor eax, eax
-  inc eax                       ; 1 (write)
-  mov edi, eax                  ; 1 (stdout)
 
-  mov esi, message ; message
-  xor edx, edx
-  mov dl, message_len
-  syscall
-
-  xor eax, eax                  ; 0 (read)
-  xor edi, edi                  ; 0 (stdin)
-  mov esi, input
-  mov edx, 2
-  syscall
-
+exit:  
   ; re-enable buffering
   or dword [termios_buf + termios.c_lflag], 0b1010 ; CANON + ECHO
-  xor esi, esi
-  inc esi
+  mov esi, TCSETS
   call tcgetorset
+
+  mov esi, bye
+  mov edx, bye_len
+  call print
   
   mov al, 60
   xor edi, edi
   syscall
 
+;; Execute TCGETS or TCSETS from/into `termios_buf`
+;; Parameters: esi (either TCGETS or TCSETS)
 tcgetorset:
   mov eax, 16                   ; IOCTL
   xor edi, edi                  ; 0 (stdin)
-  add esi, 21505                ; TCGETS if 0, TCSETS if 1
   mov edx, termios_buf
   syscall
   ret
 
+;; Get a single character from STDIN into `eax`
+getc:
+  push 0
+  xor eax, eax                  ; 0 (read)
+  xor edi, edi                  ; 0 (stdin)
+  mov rsi, rsp
+  mov edx, 1                    ; 1 char
+  syscall
+  pop rax
+  ret
+
+;; Print a string to the screen
+;; Parameters: esi (str pointer), edx/dl (str len)
+print:
+  xor eax, eax
+  inc eax                       ; 1 (write)
+  mov edi, eax                  ; 1 (stdout)
+  syscall
+  ret
+
 section .data
-message: db `\033[36m\033[1mWelcome to Solitaire!\033[m\nPlay: space\nExit: q\n> `
-message_len: equ $ - message
+welcome: db `\033[?25l\033[H\033[J\033[36mSolitaire:\033[m\n[⏎] Play [q] Exit`
+welcome_len: equ $ - welcome
+bye: db `\nBye\033[?25h\n`
+bye_len: equ $ - bye
 section .bss
-input: resb 2
 termios_buf: resb termios_size
+; 24 length deck
+deck: rep 24 db 1
+  
   
